8f7125885510d1b921c303c06950b982b7f7f756
Dipak Singh <91366857+DScharm00@users.noreply.github.com>
2022-06-23 21:11:30 +0530

    Add files via upload

---
+++ Arrays_two_D.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+/**
+ * Arrays_two_D
+ */
+public class Arrays_two_D {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt( );
+        int cols = sc.nextInt( );
+
+
+        int[][] numbers = new int[rows][cols];
+
+
+        //input user rows and columns :
+
+        //input rows
+        for(int i=0; i<rows; i++) {
+            //input columns :
+            for(int j=0; j<cols; j++) {
+                numbers[i][j] = sc.nextInt();
+            }
+        }
+        //output user rows and columns :
+        for(int i=0; i<rows; i++) {
+            for(int j=0; j<cols; j++) {
+                System.out.print(numbers[i][j] + "  ");
+            }
+            
+            System.out.println();
+
+        }
+    }
+}
